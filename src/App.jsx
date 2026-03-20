a0de8e4046a9389ec297eb2114ec9f89253513ce
tahirainam <cattblackwhite@gmail.com>
2026-03-20 08:35:52 -0700

    tailwind css setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import "./index.css";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        <h1>Noise</h1>
+        <h1 className='text-3xl font-bold underline text-center'>Noise</h1>
     </>
   )
 }
