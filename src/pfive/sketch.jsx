c0470bf9e6da0688a1214d5fa76f2057d4aab60a
tahirainam <cattblackwhite@gmail.com>
2026-03-23 02:40:16 -0700

    added first sketch

--- src/pfive/sketch.jsx
+++ src/pfive/sketch.jsx
@@ -1,9 +1,35 @@
-function sketch(){
-    return(
-        <>
-          <h1>SKETCH HERE</h1>
-        </>
-    )
-}
+import React, { useRef, useEffect } from "react";
+import p5 from "p5";
 
-export default sketch;
+const Sketch = () => {
+  const sketchRef = useRef();
+
+  useEffect(() => {
+    // Define the p5 sketch
+    const sketch = (p) => {
+      
+
+      p.setup = () => {
+        p.createCanvas(1000, 280);
+      };
+
+      p.draw = () => {
+        p.noFill();
+        p.stroke(0);
+        p.circle(p.random(p.width), p.random(p.height), 5, 5);
+      };
+    };
+
+    // Create p5 instance
+    const p5Instance = new p5(sketch, sketchRef.current);
+
+    // Cleanup on unmount
+    return () => {
+      p5Instance.remove();
+    };
+  }, []);
+
+  return <div ref={sketchRef}><a className="px-2 bg-black text-white" href="https://www.google.com" target="_blank" rel="noopener noreferrer">sketch-read</a></div>;
+};
+
+export default Sketch;
