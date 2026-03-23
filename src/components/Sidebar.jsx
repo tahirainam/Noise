d897ac5b65b8c518b305f1529d9ef095e63bd7e5
tahirainam <cattblackwhite@gmail.com>
2026-03-23 03:16:29 -0700

    added sidebar hover

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,12 @@
 function Sidebar({activeArt, onSelect}) {
   return (
     <>
-      <aside className="overflow-y-auto [&::-webkit-scrollbar]:hidden flex flex-1 flex-col mt-1 mb-1 border-l-2 border-dashed h-150 border-gray-400 text-black px-4">
-        <div className="mb-2 border-b w-full p-2 text-left hover:border border-dashed border-gray-400 font-normal font-bold">
+      <aside className="overflow-y-auto [&::-webkit-scrollbar]:hidden flex flex-1 flex-col mt-1 mb-1 border-l-2 border-dashed h-150 border-gray-400 text-black px-2">
+        <div className="hover:bg-black hover:text-white hover:border-2 hover:border-white mb-2 border-b w-full p-2 text-left hover:border border-dashed border-gray-400 font-normal font-bold ">
           <button className="cursor-pointer" onClick={() => onSelect("Sketch")}>sketch</button>
         </div>
-        <div className="mb-2 border-b w-full p-2 text-left hover:border border-dashed border-gray-400 font-normal font-bold">
-          <button className="cursor-pointer" onClick={() => onSelect("AnotherArt")}>ANOTHER ART</button>
+        <div className="hover:bg-black hover:text-white hover:border-2 hover:border-white mb-2 border-b w-full p-2 text-left hover:border border-dashed border-gray-400 font-normal font-bold ">
+          <button className="cursor-pointer" onClick={() => onSelect("AnotherArt")}>another art</button>
         </div>
       </aside>
     </>
