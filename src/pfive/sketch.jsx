fe17079f8903d6abe72e05585c22220e56fb51d4
tahirainam <cattblackwhite@gmail.com>
2026-05-19 20:48:42 -0700

    added framerate to fix

--- src/pfive/sketch.jsx
+++ src/pfive/sketch.jsx
@@ -7,10 +7,9 @@ const Sketch = () => {
   useEffect(() => {
     // Define the p5 sketch
     const sketch = (p) => {
-      
-
       p.setup = () => {
         p.createCanvas(1000, 280);
+        p.frameRate(30);
       };
 
       p.draw = () => {
@@ -20,16 +19,33 @@ const Sketch = () => {
       };
     };
 
-    // Create p5 instance
-    const p5Instance = new p5(sketch, sketchRef.current);
+    let p5Instance;
+
+    // Small delay prevents StrictMode double-mount race condition
+    const timer = setTimeout(() => {
+      p5Instance = new p5(sketch, sketchRef.current);
+    }, 0);
 
     // Cleanup on unmount
     return () => {
-      p5Instance.remove();
+      clearTimeout(timer);
+      if (p5Instance) p5Instance.remove();
     };
   }, []);
 
-  return (<div className="flex flex-col"><a className="w-25 mb-1 px-2 bg-black text-white" href="https://www.google.com" target="_blank" rel="noopener noreferrer">sketch-read</a><div className="border-2 border-black" ref={sketchRef}></div></div>);
+  return (
+    <div className="flex flex-col">
+      <a
+        className="w-25 mb-1 px-2 bg-black text-white"
+        href="https://www.google.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        sketch-read
+      </a>
+      <div className="border-2 border-black" ref={sketchRef}></div>
+    </div>
+  );
 };
 
 export default Sketch;
